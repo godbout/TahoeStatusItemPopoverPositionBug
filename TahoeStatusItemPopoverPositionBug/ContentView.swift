1c0bd07f8c71a521e47452a7ff4c5d3a6326824e
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-10-05 15:20:03 +0800

    init

---
+++ TahoeStatusItemPopoverPositionBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TahoeStatusItemPopoverPositionBug
+//
+//  Created by Guillaume Leclerc on 05/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TahoeStatusItemPopoverPositionBug/TahoeStatusItemPopoverPositionBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahoeStatusItemPopoverPositionBugApp.swift
+//  TahoeStatusItemPopoverPositionBug
+//
+//  Created by Guillaume Leclerc on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TahoeStatusItemPopoverPositionBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
